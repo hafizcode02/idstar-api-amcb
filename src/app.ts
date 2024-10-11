4eca4f32caddd1ffcb0652e9cbb7c4ff5b3b12e5
Hafiz Caniago <hafizcode02@gmail.com>
2024-10-11 14:22:55 +0700

    feat: add dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,24 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.json({ message: "IDStar API AMCB" });
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  console.log("=====================================");
+  console.log("Available routes:");
+  app._router.stack.forEach((r: any) => {
+    if (r.route && r.route.path) {
+      console.log(`http://localhost:${PORT}${r.route.path}`);
+    }
+  });
+  console.log("=====================================");
 });
